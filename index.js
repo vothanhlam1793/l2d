2b77c9fafb7c7f6df2622a170e1ec97600e22e68
Tal Tal <vothanhlam1793@gmail.com>
2024-12-16 23:23:37 +0700

    update

--- index.js
+++ index.js
@@ -7,94 +7,105 @@ const path = require('path')
 
 // Configure Environment
 dotenv.config()
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 // Configure app
-app.use(logger('dev'));
-app.set('views', __dirname + "/views");
+app.use(logger('dev'))
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
 app.get('/', (req, res) => {
-  res.render("index");
-});
-app.get("/janus", function(req,res){
-  res.render("janus");
+  res.render('index')
+})
+app.get('/janus', function (req, res) {
+  res.render('janus')
 })
 
-app.get("/control", (req, res)=>{
-  console.log("control");
-  res.render("control_device");
+app.get('/control', (req, res) => {
+  console.log('control')
+  res.render('control_device')
 })
 
-app.get("/l2d", function(req, res){
-  res.render("l2d", {
+app.get('/l2d', function (req, res) {
+  res.render('l2d', {
     bot: req.query.bot,
     map: req.query.map
-  });
+  })
 })
 
 app.get('/css', (req, res) => {
-  res.render("example");
+  res.render('example')
 })
 
 app.get('/map', (req, res) => {
-  res.render("map");
+  res.render('map')
 })
 
 app.get('/map/:version', (req, res) => {
-  res.render("map");
+  res.render('map')
 })
 
-app.get("/draw", function(req, res){
+app.get('/draw', function (req, res) {
   res.render('draw', {
-    index: req.query.index || "draw3"
-  });
-});
+    index: req.query.index || 'draw3'
+  })
+})
 
-app.get("/lz", function(req, res){
+app.get('/lz', function (req, res) {
   res.render('lz', {
-    index: req.query.index || "draw3"
-  });
-});
+    index: req.query.index || 'draw3'
+  })
+})
 
-app.get("/draw3", function(req, res){
+app.get('/draw3', function (req, res) {
   res.render('draw3', {
-    index: req.query.index || "draw3",
+    index: req.query.index || 'draw3',
     up: req.query.up || 45,
     down: req.query.down || 20
-  });
-});
-app.get("/draw4", function(req, res){
-  var render = req.query.render || "/draw4";
-  console.log("READER:", render);
+  })
+})
+app.get('/draw4', function (req, res) {
+  var render = req.query.render || '/draw4'
+  console.log('READER:', render)
   res.render(render, {
-    index: req.query.index || "draw3",
+    index: req.query.index || 'draw3',
     up: req.query.up || 45,
-    down: req.query.down || 20,
-  });
-});
+    down: req.query.down || 20
+  })
+})
 
-app.get("/drawv", function(req, res){
+app.get('/drawv', function (req, res) {
   res.render('drawv', {
-    index: req.query.index || "draw3"
-  });
-});
+    index: req.query.index || 'draw3'
+  })
+})
 
-app.get("/draw2", function(req, res){
-  res.render("draw2");
-});
+app.get('/draw2', function (req, res) {
+  res.render('draw2')
+})
 
-app.use(function(req, res, next){
-  console.log("NEX")
-  res.render("index");
-});
+app.get('/hanoi.php', function (req, res) {
+  res.redirect('/l2d/?map=map001&bot=web004')
+})
+
+app.get('/saigon.php', function (req, res) {
+  res.redirect('/l2d/?map=map001&bot=web005')
+})
+
+app.get('/dalat.php', function (req, res) {
+  res.redirect('/l2d/?map=map001&bot=web006')
+})
+
+app.use(function (req, res, next) {
+  console.log('NEX')
+  res.redirect('/l2d/?map=map001&bot=web003')
+})
 
 app.listen(port, () => {
-  console.log(`Server ${process.env.APP_NAME || "Noname"} ${port}`)
+  console.log(`Server ${process.env.APP_NAME || 'Noname'} ${port}`)
 })
